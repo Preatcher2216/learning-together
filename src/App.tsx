b7665e55b9a962f3c3b50687f10e6ae8ac9f7329
i.veshkin <i.veshkin@inostudio.com>
2023-01-12 10:40:31 +0300

    Delete unused code

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+    const increment = () => setCount((count) => count + 1)
+
   return (
     <div className="App">
-      Hello
+      Hello count {count}
+        <button onClick={increment}>Hello ;)</button>
     </div>
   )
 }
